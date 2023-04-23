e1e81c4ac814305125d6c5afd6924ff8d9f0de34
Caraline Bruzinski <cb4904@log-1.hpc.nyu.edu>
2023-04-22 20:57:52 -0400

    working serial send, need to convert to allscatter

--- scan_mpi.cpp
+++ scan_mpi.cpp
@@ -11,6 +11,7 @@ int main(int argc, char * argv[]) {
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Comm comm = MPI_COMM_WORLD;
 
   /* get name of host running MPI process */
   char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -23,41 +24,50 @@ int main(int argc, char * argv[]) {
   sscanf(argv[1], "%d", &N);
 
   // set up the data
-  long* A = (long*) malloc(N * sizeof(long));
-  long* B1 = (long*) malloc(N * sizeof(long));
+  //long* A = (long*) malloc(N * sizeof(long));
+  //long* B1 = (long*) malloc(N * sizeof(long));
 
   // for (long i = 0; i < N; i++) A[i] = rand();
-  for (long i = 0; i < N; i++) A[i] = i;
-  for (long i = 0; i < N; i++) B1[i] = 0;
+  //for (long i = 0; i < N; i++) A[i] = i;
+  //for (long i = 0; i < N; i++) B1[i] = 0;
 
+
+  int length_each = N/world_size;
+  printf("length each: %d \n", length_each);
+  
   /* timing */
-  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Barrier(comm);
   double tt = MPI_Wtime();
 
 
   // split up the array into world_size sections
-  int length_each = N/world_size;
-  printf("length each: %d \n");
 
   if (rank == 0) {
+
+	  // create a array
+	  int* A = (int*) malloc(N * sizeof(int));
+	  for (int i = 0; i < N; i++) A[i] = i;
 
 	  // send each section to the other ranks
 	  for (int process=1; process<world_size; process++) {
 
-		  // take a chunk
-		  // from 5 ... 10
 	 	printf("process %d gets indices: \n", process);
-	        long* subarray = (long*) malloc(length_each * sizeof(long));
-		for (long i = 0; i < length_each; i++) {
+	        int* subarray = (int*) malloc(length_each * sizeof(int));
+		for (int i = 0; i < length_each; i++) {
 			subarray[i] = A[length_each*process + i];	
 			printf("\t %lu\n", subarray[i]);
 		}
 
+        	for (int j=0; j<length_each; j++) {
+                	printf("\t\t Rank %d sending %d\n ", rank, subarray[j]);
+        	}
 
 		// send it to the correct process
 		int send_to_rank = process;
-		MPI_Send(subarray, length_each, MPI_INT, send_to_rank, 999, MPI_COMM_WORLD);
+		MPI_Send(subarray, length_each, MPI_INT, send_to_rank, send_to_rank, MPI_COMM_WORLD);
+		free(subarray);
 	  }
+
 	  // do your own work
 	  //
 	  // then wait for them to respond
@@ -67,8 +77,10 @@ int main(int argc, char * argv[]) {
 	int receive_from_rank = 0;
 	MPI_Status status;
 
-	long* sub = (long*) malloc(length_each * sizeof(long));
-	MPI_Recv(sub, length_each, MPI_INT, receive_from_rank, 999, MPI_COMM_WORLD, &status);
+	int* sub = (int*) malloc(length_each * sizeof(int));
+	//static int buffer[length_each];
+
+	MPI_Recv(sub, length_each, MPI_INT, receive_from_rank, rank, MPI_COMM_WORLD, &status);
 	printf("Received from rank 0:\n");
 	for (int j=0; j<length_each; j++) {
 
@@ -78,6 +90,7 @@ int main(int argc, char * argv[]) {
 	// do your work
 	// send it back
 
+	free(sub);
   }
 
 
